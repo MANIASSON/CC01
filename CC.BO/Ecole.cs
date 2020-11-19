530e6cdbb9c2386bf443e4963319bf1a6e802d3e
Nkayoum <fares.nkayoum@2tcorp.org>
2020-11-19 16:54:19 +0100

    BO

---
+++ CC.BO/Ecole.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC.BO
+{
+    class Ecole
+    {
+        public string NomEcole { get; set; }
+        public string Localisation { get; set; }
+        public string BoitePostale { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+
+        public string Logos { get; set; }
+
+        public Ecole()
+        {
+
+        }
+
+        public Ecole(string nomEcole, string localisation, string boitePostale,
+                 string telephone, string email, string logos)
+        {
+            NomEcole = nomEcole;
+            Localisation = localisation;
+            BoitePostale = boitePostale;
+            Telephone = telephone;
+            Email = email;
+            Logos = logos;
+        }
+    }
+}
---
+++ CC.BO/Etudiants.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC.BO
+{
+    public class Etudiants
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string DateNaissance { get; set; }
+        public string Nationalite { get; set; }
+        public string Identifiant { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+
+        public Etudiants()//Pour serialisation
+        {
+
+        }
+
+        public Etudiants(string nom, string prenom, string dateNaissance, string nationalite, string identifiant,
+                string telephone, string email)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            DateNaissance = dateNaissance;
+            Nationalite = nationalite;
+            Identifiant = identifiant;
+            Telephone = telephone;
+            Email = email;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Etudiants etudiants &&
+                   Identifiant == etudiants.Identifiant;
+        }
+
+        public override int GetHashCode()
+        {
+            return 574969646 + EqualityComparer<string>.Default.GetHashCode(Identifiant);
+        }
+
+       
+    }
+}
