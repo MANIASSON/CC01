e6f9112ad6a095d87317056f040b40714eb30b24
MANIASSON_RICHELLE <MANIASSON_RICHELLE@LAPTOP-J6IQ8TEI>
2020-11-19 14:09:44 +0100

    Design

---
+++ CC01.WinForms/Etudiant.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace CC01.WinForms
+{
+    partial class Etudiant
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textLIEUDENAISSANCE = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textEMAIL = new System.Windows.Forms.TextBox();
+            this.textTELEPHONE = new System.Windows.Forms.TextBox();
+            this.textIDENTIFIANT = new System.Windows.Forms.TextBox();
+            this.textNATIONALITE = new System.Windows.Forms.TextBox();
+            this.textDATEDENAISSANCE = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textPRENOM = new System.Windows.Forms.TextBox();
+            this.textNOM = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textLIEUDENAISSANCE
+            // 
+            this.textLIEUDENAISSANCE.Location = new System.Drawing.Point(48, 303);
+            this.textLIEUDENAISSANCE.Multiline = true;
+            this.textLIEUDENAISSANCE.Name = "textLIEUDENAISSANCE";
+            this.textLIEUDENAISSANCE.Size = new System.Drawing.Size(231, 36);
+            this.textLIEUDENAISSANCE.TabIndex = 35;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(45, 268);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(156, 16);
+            this.label8.TabIndex = 34;
+            this.label8.Text = "LIEU DE NAISSANCE";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(398, 389);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 38);
+            this.button2.TabIndex = 33;
+            this.button2.Text = "CANCEL";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // textEMAIL
+            // 
+            this.textEMAIL.Location = new System.Drawing.Point(332, 303);
+            this.textEMAIL.Multiline = true;
+            this.textEMAIL.Name = "textEMAIL";
+            this.textEMAIL.Size = new System.Drawing.Size(245, 36);
+            this.textEMAIL.TabIndex = 32;
+            // 
+            // textTELEPHONE
+            // 
+            this.textTELEPHONE.Location = new System.Drawing.Point(332, 216);
+            this.textTELEPHONE.Multiline = true;
+            this.textTELEPHONE.Name = "textTELEPHONE";
+            this.textTELEPHONE.Size = new System.Drawing.Size(245, 37);
+            this.textTELEPHONE.TabIndex = 31;
+            // 
+            // textIDENTIFIANT
+            // 
+            this.textIDENTIFIANT.Location = new System.Drawing.Point(332, 130);
+            this.textIDENTIFIANT.Multiline = true;
+            this.textIDENTIFIANT.Name = "textIDENTIFIANT";
+            this.textIDENTIFIANT.Size = new System.Drawing.Size(245, 38);
+            this.textIDENTIFIANT.TabIndex = 30;
+            // 
+            // textNATIONALITE
+            // 
+            this.textNATIONALITE.Location = new System.Drawing.Point(332, 52);
+            this.textNATIONALITE.Multiline = true;
+            this.textNATIONALITE.Name = "textNATIONALITE";
+            this.textNATIONALITE.Size = new System.Drawing.Size(245, 35);
+            this.textNATIONALITE.TabIndex = 29;
+            // 
+            // textDATEDENAISSANCE
+            // 
+            this.textDATEDENAISSANCE.Location = new System.Drawing.Point(48, 216);
+            this.textDATEDENAISSANCE.Multiline = true;
+            this.textDATEDENAISSANCE.Name = "textDATEDENAISSANCE";
+            this.textDATEDENAISSANCE.Size = new System.Drawing.Size(231, 37);
+            this.textDATEDENAISSANCE.TabIndex = 28;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(86, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 38);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "ENTRER";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // textPRENOM
+            // 
+            this.textPRENOM.Location = new System.Drawing.Point(48, 130);
+            this.textPRENOM.Multiline = true;
+            this.textPRENOM.Name = "textPRENOM";
+            this.textPRENOM.Size = new System.Drawing.Size(231, 38);
+            this.textPRENOM.TabIndex = 26;
+            // 
+            // textNOM
+            // 
+            this.textNOM.Location = new System.Drawing.Point(48, 52);
+            this.textNOM.Multiline = true;
+            this.textNOM.Name = "textNOM";
+            this.textNOM.Size = new System.Drawing.Size(231, 35);
+            this.textNOM.TabIndex = 25;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(329, 182);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 16);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "TELEPHONE";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(329, 268);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 16);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "EMAIL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(329, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 16);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "IDENTIFIANT";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(329, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 16);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "NATIONALITE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(48, 182);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(164, 16);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "DATE DE NAISSANCE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 102);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(73, 16);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "PRENOM";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 16);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "NOM";
+            // 
+            // Etudiant
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(625, 448);
+            this.Controls.Add(this.textLIEUDENAISSANCE);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textEMAIL);
+            this.Controls.Add(this.textTELEPHONE);
+            this.Controls.Add(this.textIDENTIFIANT);
+            this.Controls.Add(this.textNATIONALITE);
+            this.Controls.Add(this.textDATEDENAISSANCE);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textPRENOM);
+            this.Controls.Add(this.textNOM);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Etudiant";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textLIEUDENAISSANCE;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textEMAIL;
+        private System.Windows.Forms.TextBox textTELEPHONE;
+        private System.Windows.Forms.TextBox textIDENTIFIANT;
+        private System.Windows.Forms.TextBox textNATIONALITE;
+        private System.Windows.Forms.TextBox textDATEDENAISSANCE;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textPRENOM;
+        private System.Windows.Forms.TextBox textNOM;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
--- CC01.WinForms/Etudiant.cs
+++ CC01.WinForms/Etudiant.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CC01.WinForms
 {
-    public partial class Form1 : Form
+    public partial class Etudiant : Form
     {
-        public Form1()
+        public Etudiant()
         {
             InitializeComponent();
         }
